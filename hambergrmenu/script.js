79cdccce93595582a5011daf17fe3f62f6759e86
Srutidev Barman <srutidevbarman2002@gmail.com>
2023-11-12 21:14:32 +0530

    added stickey navbar

--- hambergrmenu/script.js
+++ hambergrmenu/script.js
@@ -1,8 +1,21 @@
 let listItems = document.querySelector(".list-items")
 let hamBerger = document.querySelector(".hamberger")
+let navStickey = document.querySelector("#nav")
 
-
-hamBerger.addEventListener("click",()=>{
+hamBerger.addEventListener("click", () => {
     hamBerger.classList.toggle("active")
     listItems.classList.toggle("active")
 })
+
+window.addEventListener("scroll", () => {
+    if (window.outerWidth > 430) {
+
+        navStickey.classList.toggle("stickey", window.scrollY > 0)
+        navStickey.classList.remove("stickeyNot")
+    }
+    else {
+        navStickey.classList.add("stickeyNot")
+    }
+})
+
+
